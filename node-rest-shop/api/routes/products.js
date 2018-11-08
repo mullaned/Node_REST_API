adbde61a1ad58fcbf298e0dfdc26abb9208bdb99
David M <mullaned@gmail.com>
2018-11-08 22:16:20 +0000

    add check auth to routes I want to protect

--- node-rest-shop/api/routes/products.js
+++ node-rest-shop/api/routes/products.js
@@ -134,7 +134,7 @@ router.get("/:productId", (req, res, next) => {
     });
 });
 
-router.patch("/:productId", (req, res, next) => {
+router.patch("/:productId",  checkAuth, (req, res, next) => {
   const id = req.params.productId;
   const updateOps = {};
   for (const ops of req.body) {
@@ -159,7 +159,7 @@ router.patch("/:productId", (req, res, next) => {
     });
 });
 
-router.delete("/:productId", (req, res, next) => {
+router.delete("/:productId",  checkAuth, (req, res, next) => {
   const id = req.params.productId;
   Product.remove({ _id: id })
     .exec()
