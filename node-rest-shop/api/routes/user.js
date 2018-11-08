23e135bfe8f1a7fe39963ba5e565061480c968c3
David M <mullaned@gmail.com>
2018-11-08 22:49:10 +0000

    add check auth to delete user

--- node-rest-shop/api/routes/user.js
+++ node-rest-shop/api/routes/user.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+//import check auth middleware
+const checkAuth = require('../middleware/check-auth');
+
 //import controllers
 const UserController = require('../controllers/user');
 
@@ -8,8 +11,7 @@ router.post('/signup', UserController.user_signup);
 
 router.post('/login', UserController.user_login);
 
-
-router.delete('/:userId', UserController.user_delete);
+router.delete('/:userId', checkAuth, UserController.user_delete);
 
 
 module.exports = router;
